6dd40c657a9862adb61b019a2ddbec625d8539e1
Alexandre Brasil <alexandre.brazil.official@gmail.com>
2024-08-13 15:57:41 +0100

    feat: footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -31,24 +31,6 @@ const socialLinks: socialLinks[] = [
   },
 ];
 
-const products: ProductType[] = [
-  {
-    id: 1,
-    section: "Company",
-    link: ["About", "Careers", "Mobile", "Blog", "How we work?"],
-  },
-  {
-    id: 2,
-    section: "Contact",
-    link: ["Help/FAQ", "Press", "Affiliates", "Hotel owners", "Partners"],
-  },
-  {
-    id: 3,
-    section: "More",
-    link: ["Recipe", "Chef", "Food", "Support"],
-  },
-];
-
 const footer = () => {
   return (
     <div className="mx-auto max-w-2xl pt-24 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
@@ -69,8 +51,7 @@ const footer = () => {
           </div>
           <h3 className="text-textbl text-xs font-medium mt-5 mb-4 lg:mb-16">
             {" "}
-            Open an account in minutes, get full financial <br /> control for
-            much longer.
+            Acompanhe-nos em nossas redes <strong>Sociais.</strong>
           </h3>
           <div className="flex gap-4">
             {socialLinks.map((items, i) => (
@@ -90,31 +71,11 @@ const footer = () => {
         </div>
 
         {/* CLOUMN-2/3/4 */}
-
-        {products.map((product) => (
-          <div key={product.id} className="sm:col-span-2">
-            <p className="text-black text-xl font-semibold mb-9">
-              {product.section}
-            </p>
-            <ul>
-              {product.link.map((link: string, index: number) => (
-                <li key={index} className="mb-5">
-                  <Link
-                    href="/"
-                    className="text-footerlinks text-base font-normal mb-6 space-links"
-                  >
-                    {link}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
       </div>
 
       {/* All Rights Reserved */}
 
-      <div className="py-10 md:flex items-center justify-between border-t border-t-bordertop">
+      <div className="py-10 md:flex items-center  border-t border-t-bordertop">
         <h4 className="text-darkgrey text-sm text-center md:text-start font-normal">
           Enersol©. All Rights Reserved by{" "}
           <Link href="https://alexandre.agency/" target="_blank">
@@ -122,19 +83,6 @@ const footer = () => {
             Alexandre Brasil
           </Link>
         </h4>
-        <div className="flex gap-5 mt-5 md:mt-0 justify-center md:justify-start">
-          <h4 className="text-darkgrey text-sm font-normal">
-            <Link href="/" target="_blank">
-              Privacy policy
-            </Link>
-          </h4>
-          <div className="h-5 bg-bordertop w-0.5"></div>
-          <h4 className="text-darkgrey text-sm font-normal">
-            <Link href="/" target="_blank">
-              Terms & conditions
-            </Link>
-          </h4>
-        </div>
       </div>
     </div>
   );
